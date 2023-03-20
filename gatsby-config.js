5da4f0ad41daf37045f1b526d5ac17454eca1f4b
ovidiujordache <ovi_jordache@protonmail.com>
2023-03-20 12:11:01 +0000

    removed public

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
+module.exports = {
+    siteMetadata: {
+        title: `gtsapp1`,
+        siteUrl: `https://www.yourdomain.tld`
+    },
+    plugins: ["gatsby-plugin-styled-components",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sitemap", {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                "icon": "src/images/icon.png"
+            }
+        }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        }, {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "pages",
+                "path": "./src/pages/"
+            },
+            __key: "pages"
+        }
+    ]
+};
